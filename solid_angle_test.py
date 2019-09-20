762f12435e5b61f7681532211b41d986af58c459
Marc Leroy <marc.leroy@pix4d.com>
2019-09-20 10:11:23 +0200

    Added wireframe history

--- solid_angle_test.py
+++ solid_angle_test.py
@@ -7,8 +7,12 @@ from mpl_toolkits.mplot3d import axes3d
 
 
 N_SECT_2D = 12
+I_SECT_2D = 360./N_SECT_2D
 N_SECT_3D = 8
-N_WIREFRAME = 5
+I_SECT_3D = 180./N_SECT_3D
+N_WIREFRAME = 3
+N_SECT_TOT = N_SECT_2D*N_SECT_3D
+SECT_PASSED = np.zeros((N_SECT_2D, N_SECT_3D), dtype=bool)
 
 def sph2car(r, phi, theta):
     x = r*np.sin(np.radians(theta))*np.cos(np.radians(phi))
@@ -19,7 +23,7 @@ def sph2car(r, phi, theta):
 
 def plot_all(ax, X, Y, Z, px, py, pz):
     # Plot a 3D surface
-    ax.plot_wireframe(X, Y, Z)
+    ax.plot_wireframe(X, Y, Z, colors="green")
     ax.plot([0, px], [0, py], [0, pz], c="green")
     ax.scatter(px, py, pz, s=50, c="green")
 
@@ -37,7 +41,7 @@ def get_interval(l, v):
     for i in range(len(l)-1):
         if l[i] <= v <= l[i+1]:
             print("{} is between {} and {}".format(v, l[i], l[i+1]))
-            return l[i], l[i+1]
+            return l[i], l[i+1], i
     print("Error")
     sys.exit()
 
@@ -46,6 +50,7 @@ def main():
     phi_l = np.linspace(0., 360., N_SECT_2D+1) - 360./(2*N_SECT_2D)
     theta_l = np.linspace(0., 180., N_SECT_3D+1)
     print(phi_l)
+    print(theta_l)
 
     r = 100
     phi_init = 60
@@ -62,11 +67,13 @@ def main():
     stheta = Slider(axtheta, 'Polar angle (theta)', 0, 180,
                     valinit=theta_init, valstep=1, color="blue")
 
-    pa, pb = get_interval(phi_l, phi_init)
+    pa, pb, p_i = get_interval(phi_l, phi_init)
     phi = np.linspace(pa, pb, N_WIREFRAME+1)
 
-    ta, tb = get_interval(theta_l, theta_init)
+    ta, tb, t_i = get_interval(theta_l, theta_init)
     theta = np.linspace(ta, tb, N_WIREFRAME+1)
+
+    SECT_PASSED[p_i, t_i] = True
 
     Phi, Theta = np.meshgrid(phi, theta)
     X, Y, Z = sph2car(r, Phi, Theta)
@@ -77,12 +84,24 @@ def main():
         ax.clear()
 
         new_phi = sphi.val
-        pl, pr = get_interval(phi_l, new_phi)
+        pl, pr, p_i = get_interval(phi_l, new_phi)
         phi = np.linspace(pl, pr, N_WIREFRAME+1)
 
         new_theta = stheta.val
-        tl, tr = get_interval(theta_l, new_theta)
+        tl, tr, t_i = get_interval(theta_l, new_theta)
         theta = np.linspace(tl, tr, N_WIREFRAME+1)
+
+        SECT_PASSED[p_i, t_i] = True
+        # print(SECT_PASSED)
+        for i in range(SECT_PASSED.shape[0]):
+            for j in range(SECT_PASSED.shape[1]):
+                if SECT_PASSED[i, j]:
+                    old_phi = np.linspace(i*I_SECT_2D - 360./(2*N_SECT_2D), (i+1)*I_SECT_2D - 360./(2*N_SECT_2D), N_WIREFRAME+1)
+                    old_theta = np.linspace(j*I_SECT_3D, (j+1)*I_SECT_3D, N_WIREFRAME+1)
+                    old_Phi, old_Theta = np.meshgrid(old_phi, old_theta)
+                    X, Y, Z = sph2car(r, old_Phi, old_Theta)
+                    ax.plot_wireframe(X, Y, Z, colors="blue", alpha=0.1)
+
         Phi, Theta = np.meshgrid(phi, theta)
         X, Y, Z = sph2car(r, Phi, Theta)
 
